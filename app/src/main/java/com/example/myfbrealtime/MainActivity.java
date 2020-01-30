131a19462c01844c5715e8c483f145490a4c5e49
Docta Rhyf <drrhyf@gmail.com>
2020-01-31 00:28:21 +0200

    real time database working on all devices

--- app/src/main/java/com/example/myfbrealtime/MainActivity.java
+++ app/src/main/java/com/example/myfbrealtime/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -50,6 +51,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.e(TAG, "onCancelled: ->  " + databaseError.getMessage());
+            }
+        });
+
+        mButtonSunny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("Sunny");
+            }
+        });
+
+        mButtonFoggy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mConditionRef.setValue("Foggy");
             }
         });
 
