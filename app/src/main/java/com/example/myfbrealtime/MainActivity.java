dc9c5b14af214a9975598a37d3b0ab10eba90a95
Docta Rhyf <drrhyf@gmail.com>
2020-01-31 00:23:38 +0200

    real time database working

--- app/src/main/java/com/example/myfbrealtime/MainActivity.java
+++ app/src/main/java/com/example/myfbrealtime/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.myfbrealtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MY APP";
+    TextView mTextViewCondition;
+    Button mButtonSunny, mButtonFoggy;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("condition");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCondition = findViewById(R.id.textViewCondition);
+        mButtonFoggy = findViewById(R.id.buttonFoggy);
+        mButtonSunny = findViewById(R.id.buttonSunny);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mTextViewCondition.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled: ->  " + databaseError.getMessage());
+            }
+        });
+
+
+
     }
 }
